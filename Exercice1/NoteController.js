0d1cc8c7f87e68052b9c93e5a81a597da327f667
Erwann.leroux <erwann.leroux1@gmail.com>
2017-01-24 15:52:17 +0100

    4ème Commit

--- Exercice1/NoteController.js
+++ Exercice1/NoteController.js
@@ -1,9 +1,22 @@
 /**
  * Created by Erwann on 24/01/2017.
  */
-var myApp = angular.module('Note',[]);
+var myApp = angular.module('Note',['ngCookies']);
 
-myApp.controller('NoteController', [function() {
+myApp.controller('NoteController', ["$cookies",function() {
+    var self=this;
+    this.message = "";
 
+    this.save = function(message) {
+        // Setting a cookie
+        $cookies.put('Exercice1',message);
+    };
+    this.count = function() {
+        var res = (100 - self.message.length);
+        return res;
+    };
 
+    this.clear = function(value) {
+
+    };
 }]);
