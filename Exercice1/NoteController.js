17d095f1822f92b6a760729952a92651cf2b752b
Erwann.leroux <erwann.leroux1@gmail.com>
2017-01-30 22:59:05 +0100

    Version Finale
    Tout est fonctionnel

--- Exercice1/NoteController.js
+++ Exercice1/NoteController.js
@@ -5,20 +5,40 @@ var myApp = angular.module('Note',['ngCookies']);
 
 myApp.controller('NoteController', ["$cookies",function($cookies) {
     var self=this;
-    this.message = "";
+
+    var favoriteCookie = $cookies.get('monCookie');
+
+    this.message = favoriteCookie;
+    //0 -> initial , 1 -> modifiée, 2 -> sauvegardée
+    this.status = 1;
+    this.info = "";
+
 
     this.save = function() {
-        // Setting a cookie
-        var favoriteCookie = $cookies.get('monCookie');
-        // Setting a cookie
-        $cookies.put('monCookie', self.message);
+        if(self.message != "")
+        {
+            // Setting a cookie
+            $cookies.put('monCookie', self.message);
+            self.info="Note sauvegardée";
+            self.status=2;
+        }
+
     };
     this.count = function() {
         var res = (100 - self.message.length);
+        if(res == 100)
+            self.status = 1;
         return res;
     };
 
     this.clear = function(value) {
-
+        self.message ="";
+        self.status = 1;
     };
+
+    this.change = function(){
+        self.status = 0;
+        self.info = "Note modifiée";
+    }
+
 }]);
